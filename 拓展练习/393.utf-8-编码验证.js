c6ae7029cbe6f8ed1d4e5fa6722a75c259247c97
zhang13pro <lexzhang13@qq.com>
2022-03-31 18:21:28 +0800

    chore：拓展练习

---
+++ 拓展练习/393.utf-8-编码验证.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=393 lang=javascript
+ *
+ * [393] UTF-8 编码验证
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} data
+ * @return {boolean}
+ */
+var validUtf8 = function(data) {
+
+};
+// @lc code=end
+
