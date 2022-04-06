71913b966ecb24c53ae730b10fa9f28e4cb71df3
zhang13pro <lexzhang13@qq.com>
2022-04-06 08:53:33 +0800

    chore: 挖坑6

---
+++ 图/279.完全平方数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=javascript
+ *
+ * [279] 完全平方数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+
+};
+// @lc code=end
+
---
+++ 堆/347.前-k-个高频元素.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=javascript
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+
+};
+// @lc code=end
+
