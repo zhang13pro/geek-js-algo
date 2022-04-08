90d490379493ccf8e16d0871bdcdadb851e18997
LEX <zhang13pro@qq.com>
2022-04-08 13:35:42 +0800

    chore: one more time

--- 栈/20.有效的括号.js
+++ 栈/20.有效的括号.js
@@ -9,8 +9,48 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function (s) {
+  const stack = []
+  for (const i of s) {
+    switch (i) {
+      case "(":
+        stack.push(")")
+        break
+      case "[":
+        stack.push("]")
+        break
+      case "{":
+        stack.push("}")
+        break
+      default:
+        if (i !== stack.pop()) return false
+    }
+  }
 
-};
+  return stack.length === 0
+}
+
+function isValid(s){
+  let stack = []
+  for (const i of s) {
+    if (i === "(") stack.push(")")
+    else if (i === "{") stack.push("}")
+    else if (i === "[") stack.push("]")
+    else if (stack.length === 0 || i !== stack.pop()) return false
+  }
+
+  return stack.length === 0
+}
+
+function isValid(str) {
+  let s = /\(\)/g
+  let m = /\[\]/g
+  let l = /\{\}/g
+
+  str.replace(s, "")
+  str.replace(m, "")
+  str.replace(l, "")
+
+  return str === ""
+}
 // @lc code=end
-
